e29d87d1a687231890a580db636aa8927d46776d
Donovan Keith <donovanskeith@gmail.com>
2015-08-21 13:07:23 -0700

    Added another test case.

--- test_frameio.py
+++ test_frameio.py
@@ -4,21 +4,27 @@ A simple script for testing the implementation of the Frame.io python wrapper.""
 import unittest
 import frameio
 
+email = "tester@example.com"
+password = "testing"
+
 class LoginTestCase(unittest.TestCase):
     """Tests for `frameio.py`."""
 
     def test_good_email_and_pass(self):
-        """Do we get a True response with a good login?"""
         frameio_api = frameio.FrameIO()
-        self.assertTrue(frameio_api.login(email="tester@example.com", password="testing"))
+        self.assertTrue(frameio_api.login(email=email, password=password))
 
     def test_good_email_and_bad_pass(self):
         frameio_api = frameio.FrameIO()
-        self.assertFalse(frameio_api.login(email="tester@example.com", password="sldfj"))
+        self.assertFalse(frameio_api.login(email=email, password="sldfj"))
 
     def test_no_email(self):
         frameio_api = frameio.FrameIO()
         self.assertFalse(frameio_api.login(email=""))
+
+    def test_good_email_and_no_password(self):
+        frameio_api = frameio.FrameIO()
+        self.assertFalse(frameio_api.login(email=email))
 
 if __name__ == '__main__':
     unittest.main()
