014c931594dd4877116a9a177434a834c70de969
Donovan Keith <donovanskeith@gmail.com>
2015-08-21 13:05:18 -0700

    Correct response with bad email

--- frameio.py
+++ frameio.py
@@ -41,7 +41,10 @@ class FrameIO():
         Return True if successful, False if not"""
 
         #Email Check
-        self.check_eligible(email=email)
+        if not self.check_eligible(email=email):
+            return False
+
+        self.password = password
 
         #Crap out if something goes wrong
         if self.errors:
@@ -52,11 +55,13 @@ class FrameIO():
             print "No action key, don't know how to login"
             return False
 
+        print "password: ", self.password
         print self
 
         if self.action_key == "user-non-google":
             print "Non-Google User, standard login okay"
-            #TODO: Implement
+            return self.login_user_non_google()
+
         elif self.action_key == "user-google":
             print "Google Login"
             #TODO: Implement
@@ -66,15 +71,46 @@ class FrameIO():
         else:
             print "Non-implemented action_key: ", self.action_key
 
+    def login_user_non_google(self):
+        """Login the user w/ the stored credentials.
+        Return True if successful, False if any errors."""
+
+        values = {
+            "a": self.email,
+            "b": self.password
+        }
+
+        r = requests.post("https://api.frame.io/login", values)
+        response = r.json()
+
+        print response
+
+        if "x" in response:
+            self.user_id = response["x"]
+
+        if "y" in response:
+            self.token = response["y"]
+
+        if "messages" in response:
+            self.messages = response["messages"]
+
+        if "errors" in response:
+            self.errors = response["errors"]
+            print self.errors
+            return False
+
+        return True
 
     def check_eligible(self, email=None):
         """Check to see if email address is valid. If it is, store it in the class.
 
         reference: http://docs.frameio.apiary.io/#reference/authentication/initial-mail-check/initial-mail-check
+
+        return True if eligible, False if not.
         """
 
         if not email:
-            return
+            return False
 
         #Store the provided email in the class
         self.email = email
@@ -95,3 +131,8 @@ class FrameIO():
 
         if "errors" in response:
             self.errors = response["errors"]
+
+        if not self.errors:
+            return True
+        else:
+            return False
--- test_frameio.py
+++ test_frameio.py
@@ -4,20 +4,21 @@ A simple script for testing the implementation of the Frame.io python wrapper.""
 import unittest
 import frameio
 
-email = "tester@example.com"
-password = "testing"
-
 class LoginTestCase(unittest.TestCase):
     """Tests for `frameio.py`."""
 
     def test_good_email_and_pass(self):
         """Do we get a True response with a good login?"""
         frameio_api = frameio.FrameIO()
-        self.assertTrue(frameio_api.login(email=email, password=password))
+        self.assertTrue(frameio_api.login(email="tester@example.com", password="testing"))
 
     def test_good_email_and_bad_pass(self):
         frameio_api = frameio.FrameIO()
-        self.assertFalse(frameio_api.login(email=email, password="sldfj"))
+        self.assertFalse(frameio_api.login(email="tester@example.com", password="sldfj"))
+
+    def test_no_email(self):
+        frameio_api = frameio.FrameIO()
+        self.assertFalse(frameio_api.login(email=""))
 
 if __name__ == '__main__':
     unittest.main()
