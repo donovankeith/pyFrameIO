bfbd4fb68904e29c0358cac927d74874663fecc6
Donovan Keith <donovanskeith@gmail.com>
2015-08-21 11:34:13 -0700

    Started defining the frameio class. Created simple test script.

---
+++ frameio.py
@@ -0,0 +1,7 @@
+"""frameio
+A Python Wrapper for the [Frame.io](http://www.frameio.com) API.
+"""
+
+class FrameIO():
+    def __init__(self):
+        pass
---
+++ frameio_test.py
@@ -0,0 +1,10 @@
+"""frameio_test
+A simple script for testing the implementation of the Frame.io python wrapper."""
+
+import frameio
+
+def main():
+    frameio_api = frameio.FrameIO()
+
+if __name__ == "__main__":
+    main()
